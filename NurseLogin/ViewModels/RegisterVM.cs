a3f866351835fab79b995fae62061ca9f912c3c2
tlombo <tlombo@ZAJNB5CG34970H0>
2024-05-16 21:27:08 +0200

    Add project files.

---
+++ NurseLogin/ViewModels/RegisterVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NurseLogin.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passowrd do not match.")]
+        [Display (Name = "Confirm Password")]
+		[DataType(DataType.Password)]
+		public string? ConfirmPassowrd { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string? Address { get;}
+    }
+}
